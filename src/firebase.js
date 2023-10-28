0b14c991036254d3776912aedca326ee4c34cce3
hx1228 <hauxiang123@gmail.com>
2023-10-28 17:38:21 +0800

    added firebase config
    added firebase config and some dummy posts in firebase

---
+++ src/firebase.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCX-Cs45wGotEJyUx6vvzRAomrxkC2gxrM",
+  authDomain: "bt3103-f7088.firebaseapp.com",
+  projectId: "bt3103-f7088",
+  storageBucket: "bt3103-f7088.appspot.com",
+  messagingSenderId: "1035230740642",
+  appId: "1:1035230740642:web:792dc77e0daf851e6797fb",
+};
+
+const firebaseApp = initializeApp(firebaseConfig);
+
+export default firebaseApp;
