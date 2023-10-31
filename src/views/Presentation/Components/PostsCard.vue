edb49b8b47b899bbb9fc6839e12492c57a0312bf
nathankkh <nathankkh@gmail.com>
2023-10-31 14:09:57 +0800

    Add post previews by entity for homepage.
    To-dos:
    - CSS for PostsCard to align with other pages
    - Update routers for the following
    -- View more button (posts)
    -- Read more button (PostsCard). May need to modify PostsCard to accept a link to the post as a prop
    -- Join Us button
    
    Issues:
    - PostsCard image and button to view posts are not displaying

---
+++ src/views/Presentation/Components/PostsCard.vue
@@ -0,0 +1,55 @@
+<template>
+    <div class="card">
+        <img :src="post.imageURL" class="card-img-top" alt="Post Image" />
+        <div class="card-body">
+            <h5 class="card-title">{{ post.title }}</h5>
+            <p class="card-text">{{ post.username }} | {{ post.entity }}</p>
+            <p class="card-text description">{{ post.description }}</p>
+        </div>
+        <div class="card-footer">
+            <button class="btn btn-success btn-sm">Read More</button>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['post']
+};
+</script>
+
+<style scoped>
+.card {
+    background-color: white;
+    border: 1px solid black;
+    border-radius: 10px;
+    overflow: hidden;
+    max-height: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+}
+
+.card-body {
+    display: flex;
+    flex-direction: column;
+    align-items: stretch;
+    flex: 1;
+}
+
+.card img {
+    width: 100%;
+    height: auto;
+    object-fit: cover;
+}
+
+.card-footer {
+    padding: 8px;
+    display: flex;
+    justify-content: flex-end;
+}
+
+.description {
+    margin: 0 16px 16px;
+}
+</style>
---
+++ src/views/Presentation/Sections/PostPreviewNGO.vue
@@ -0,0 +1,64 @@
+<script>
+import getRecentPosts from './Data/getPreviewPosts.js';
+import PostsCard from '../Components/PostsCard.vue';
+
+export default {
+    data() {
+        return {
+            previewPosts: []
+        };
+    },
+    methods: {
+        getPosts() {
+            getRecentPosts('NGO').then((data) => {
+                console.log(data);
+                // Update the previewPosts data property with the fetched data
+                this.previewPosts = data;
+            });
+        }
+    },
+    created() {
+        this.getPosts(); // Call the getPosts method when the component is created
+    },
+    components: {
+        PostsCard
+    }
+};
+</script>
+
+<template>
+    <div class="container rounded border border-secondary border-2 py-3">
+        <div class="column mx-4">
+            <div class="title">
+                <h2>Recent NGO Posts</h2>
+            </div>
+            <div class="card-container row justify-content-between mb-3">
+                <PostsCard
+                    v-for="(post, index) in previewPosts"
+                    :post="post"
+                    :key="index"
+                ></PostsCard>
+            </div>
+            <div class="justify-content-center row">
+                <button class="btn btn-success">View More</button>
+                <!-- <button @click="getPosts">Test me</button> -->
+            </div>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+.card {
+    width: 30%;
+    height: 200px;
+    background-color: white;
+    border: 1px solid black;
+    border-radius: 10px;
+}
+.card-container {
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+    justify-content: flex-start;
+}
+</style>
