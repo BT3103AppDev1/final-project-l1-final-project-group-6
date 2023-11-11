2ebf1338f52cd18b09061b0c975af0d8717a1402
hx1228 <hauxiang123@gmail.com>
2023-11-12 03:47:20 +0800

    Added company profile
    Added company profile when clicking on company in post page
    to do:
    implement following function
    improve css
    implement back button

--- src/views/LandingPages/ContactUs/ContactView.vue
+++ src/views/LandingPages/ContactUs/ContactView.vue
@@ -23,14 +23,11 @@ onMounted(() => {
   <div class="container position-sticky z-index-sticky top-0">
     <div class="row">
       <div class="col-12">
-        <DefaultNavbar
-          :sticky="true"
-          :action="{
-            route: '',
-            color: 'bg-gradient-success',
-            label: 'Buy Now',
-          }"
-        />
+        <DefaultNavbar :sticky="true" :action="{
+          route: '',
+          color: 'bg-gradient-success',
+          label: 'Buy Now',
+        }" />
       </div>
     </div>
   </div>
@@ -39,38 +36,21 @@ onMounted(() => {
       <div class="container">
         <div class="row">
           <div
-            class="col-6 d-lg-flex d-none h-100 my-auto pe-0 position-absolute top-0 start-0 text-center justify-content-center flex-column"
-          >
-            <div
-              class="position-relative h-100 m-3 px-7 border-radius-lg d-flex flex-column justify-content-center"
+            class="col-6 d-lg-flex d-none h-100 my-auto pe-0 position-absolute top-0 start-0 text-center justify-content-center flex-column">
+            <div class="position-relative h-100 m-3 px-7 border-radius-lg d-flex flex-column justify-content-center"
               :style="{
                 backgroundSize: 'cover',
-              }"
-              loading="lazy"
-            >
-            <video 
-    src="../../../data/video5.mp4" 
-    type="video/mp4" 
-    autoplay 
-    loop 
-    muted
-    class="min-vh-50 w-0-0 shifted-video">
-</video>
+              }" loading="lazy">
+              <video src="../../../data/video5.mp4" type="video/mp4" autoplay loop muted
+                class="min-vh-50 w-0-0 shifted-video">
+              </video>
 
+            </div>
           </div>
-          </div>
-          <div
-            class="mt-8 col-xl-5 col-lg-6 col-md-7 d-flex flex-column ms-auto me-auto ms-lg-auto me-lg-5"
-          >
-            <div
-              class="card d-flex blur justify-content-center shadow-lg my-sm-0 my-sm-6 mt-8 mb-5"
-            >
-              <div
-                class="card-header p-0 position-relative mt-n4 mx-3 z-index-2 bg-transparent"
-              >
-                <div
-                  class="bg-gradient-success shadow-success border-radius-lg p-3"
-                >
+          <div class="mt-8 col-xl-5 col-lg-6 col-md-7 d-flex flex-column ms-auto me-auto ms-lg-auto me-lg-5">
+            <div class="card d-flex blur justify-content-center shadow-lg my-sm-0 my-sm-6 mt-8 mb-5">
+              <div class="card-header p-0 position-relative mt-n4 mx-3 z-index-2 bg-transparent">
+                <div class="bg-gradient-success shadow-success border-radius-lg p-3">
                   <h3 class="text-white text-success mb-0">Contact us</h3>
                 </div>
               </div>
@@ -84,39 +64,21 @@ onMounted(() => {
                   <div class="card-body p-0 my-3">
                     <div class="row">
                       <div class="col-md-6">
-                        <MaterialInput
-                          class="input-group-static mb-4"
-                          type="text"
-                          label="Full Name"
-                          placeholder="Full Name"
-                        />
+                        <MaterialInput class="input-group-static mb-4" type="text" label="Full Name"
+                          placeholder="Full Name" />
                       </div>
                       <div class="col-md-6 ps-md-2">
-                        <MaterialInput
-                          class="input-group-static mb-4"
-                          type="email"
-                          label="Email"
-                          placeholder="nus"
-                        />
+                        <MaterialInput class="input-group-static mb-4" type="email" label="Email" placeholder="nus" />
                       </div>
                     </div>
                     <div class="form-group mb-0 mt-md-0 mt-4">
-                      <MaterialTextArea
-                        id="message"
-                        class="input-group-static mb-4"
-                        :rows="6"
-                        placeholder="Describe your problem in at least 250 characters"
-                        >How can we help you?</MaterialTextArea
-                      >
+                      <MaterialTextArea id="message" class="input-group-static mb-4" :rows="6"
+                        placeholder="Describe your problem in at least 250 characters">How can we help you?
+                      </MaterialTextArea>
                     </div>
                     <div class="row">
                       <div class="col-md-12 text-center">
-                        <MaterialButton
-                          variant="gradient"
-                          color="success"
-                          class="mt-3 mb-0"
-                          >Send Message</MaterialButton
-                        >
+                        <MaterialButton variant="gradient" color="success" class="mt-3 mb-0">Send Message</MaterialButton>
                       </div>
                     </div>
                   </div>
