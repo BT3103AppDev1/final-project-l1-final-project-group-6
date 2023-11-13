57030a4994317f5f197707049c0a2587986b6914
nicholas <niclee1219@gmail.com>
2023-11-13 18:28:22 +0800

    added error page
    catch all invalid URLs

---
+++ src/views/LandingPages/Error/ErrorView.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <h1>404 - Page Not Found</h1>
+        <p>
+            The page you are looking for might have been removed, had its name changed, or is
+            temporarily unavailable.
+        </p>
+        <router-link to="/">Return Home</router-link>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'NotFoundView'
+};
+</script>
